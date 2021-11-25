573ff20d809747008d39b702d4f42caab47441e6
munbyeongyun <dkqk1326@naver.com>
2021-11-26 07:01:32 +0900

    랜딩페이지(1)

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "2a5348caa7518ec68e5f75ecab2f0e6d";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
